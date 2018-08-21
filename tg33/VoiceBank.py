8515bad4c4238d22c95bbe7d680581f4218ca084
Rob Fowler <42362946+robmfowler@users.noreply.github.com>
2018-08-21 19:46:21 -0400

    Parse hex of sysex message

--- tg33/VoiceBank.py
+++ tg33/VoiceBank.py
@@ -2,13 +2,14 @@ import mido
 
 class VoiceBank:
     def __init__(self, sysex_file):
-        self.messages = mido.read_syx_file(sysex_file)
-        message_count = len(self.messages)
+        messages = mido.read_syx_file(sysex_file)
+        message_count = len(messages)
         if message_count != 1:
             raise ValueError(F"tg33 expects one voicebank (found {message_count}).")
+        self.message = messages[0]
 
     def __repr__(self):
         return super().__repr__()
 
     def __str__(self):
-        return "\n".join([str(message) for message in self.messages])
+        return "\n".join([hexstr for hexstr in self.message.hex().split(' ')])
--- tg33/__main__.py
+++ tg33/__main__.py
@@ -5,7 +5,7 @@ def main():
     args = sys.argv[1:]
     if len(args) != 1:
         raise ValueError("tg33 takes one argument, a TG33 voice bank")
-    VoiceBank(args[0])
+    print(str(VoiceBank(args[0])))
 
 if __name__ == '__main__':
     main()
